f814308662548338d60606dbe1cbf40b8210ebef
Kalev Keil <103848034+kodev8@users.noreply.github.com>
2025-01-20 00:21:44 +0100

    feat(camera): init camera + bind preview

---
+++ app/src/main/java/com/example/headsup/managers/CameraManager.java
@@ -0,0 +1,77 @@
+package com.example.headsup.managers;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.FileOutputOptions;
+import androidx.camera.video.Quality;
+import androidx.camera.video.QualitySelector;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.video.VideoRecordEvent;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CameraManager {
+    private static final String TAG = "CameraManager";
+    private final Context context;
+    private final PreviewView previewView;
+    private final ExecutorService cameraExecutor;
+    private ProcessCameraProvider cameraProvider;
+    private VideoCapture<Recorder> videoCapture;
+
+    public CameraManager(Context context, PreviewView previewView) {
+        this.context = context;
+        this.previewView = previewView;
+        this.cameraExecutor = Executors.newSingleThreadExecutor();
+    }
+
+    private void bindPreview(LifecycleOwner lifecycleOwner) {
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Recorder recorder = new Recorder.Builder().setQualitySelector(QualitySelector.from(Quality.HD)).build();
+        videoCapture = VideoCapture.withOutput(recorder);
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
+
+        try {
+            cameraProvider.unbindAll();
+
+            cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    preview,
+                    videoCapture
+            );
+
+        } catch (Exception e) {
+            Log.e(TAG, "Use case binding failed", e);
+        }
+    }
+
+
+    public void shutdown() {
+        cameraExecutor.shutdown();
+    }
+} 
