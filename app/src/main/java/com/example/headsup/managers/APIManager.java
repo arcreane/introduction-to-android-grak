c039230c199d440fd46f435f2a0ba18427ec8ff4
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2025-01-19 22:25:53 +0100

    feat: Added CardResponse models and retrofit api

---
+++ app/src/main/java/com/example/headsup/managers/APIManager.java
@@ -0,0 +1,4 @@
+package com.example.headsup.managers;
+
+public class APIManager {
+}
---
+++ app/src/main/java/com/example/headsup/models/CardResponse.java
@@ -0,0 +1,10 @@
+package com.example.headsup.models;
+
+public class CardResponse {
+    private Record record;
+
+    public Record getRecord() {
+        return record;
+    }
+}
+
